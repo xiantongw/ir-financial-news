b469a9cbd2066676e1ecddfa6106840f251d07cf
Xiantong Wang <xtwang@umich.edu>
2020-10-22 01:19:31 -0400

    add a json file describe the news searching keywords; add stock price downloader using yfinance

--- DataCollector.py
+++ DataCollector.py
@@ -4,39 +4,52 @@ from newspaper import Config
 import pandas as pd
 import nltk
 from datetime import datetime
+import yfinance as yf
+import json
+
 
 class DataCollector:
 
-    def __init__(self, keywords_list, start_time, end_time):
+    def __init__(self, ticker, keywords_list, start_time, end_time):
+        
         # configure for news downloading agent
         nltk.download('punkt')
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
         self.config = Config()
         self.config.browser_user_agent = user_agent
-
-        self.keywords_list = keywords_list
+        
+        self.ticker = ticker
+        self.price_df = None
+        self.keywords_dict = None
         self.start_time = start_time
         self.end_time = end_time
-        self.googlenews_client = GoogleNews(start=self.start_time,
-                                            end=self.end_time)
         self.news_df = None
         self.link_filter_list = ['seekingalpha']
+        # read the news searching JSON file
+        with open('keywords.json', 'r') as fobj:
+            self.keywords_dict = json.load(fobj)
+        if not self.ticker in list(self.keywords_dict.keys()):
+            raise ValueError('Ticker {:s} not supported!'.format(self.ticker))
+
+    @staticmethod
+    def gnews_date_fmt(date_in):
+        return datetime.strftime(datetime.strptime(date_in, '%Y-%m-%d'), 
+                                 '%m/%d/%Y')
 
     def search_news(self, max_page=10):
         news_list = list()
         # iterate for keywords
-        for keyword in self.keywords_list:
-            googlenews_client = GoogleNews(start=self.start_time,
-                                           end=self.end_time)
+        for keyword in self.keywords_dict[self.ticker]:
+            # GoogleNews accepcts different date format from yfinance
+            googlenews_client = GoogleNews(start=self.gnews_date_fmt(self.start_time),
+                                           end=self.gnews_date_fmt(self.end_time))
             googlenews_client.search(keyword)
             for i in range(1, max_page):
                 googlenews_client.getpage(i)
             news_list = news_list + googlenews_client.result()
         # convert to pandas dataframe and remove duplicates
         temp_df = pd.DataFrame(news_list)
-        temp_df.drop(['img'], inplace=True, axis=1)
-        temp_df.drop(['desc'], inplace=True, axis=1)
         temp_df.drop_duplicates(subset='link', inplace=True, keep='first')
         # get text from links
         content_list = list()
@@ -50,17 +63,21 @@ class DataCollector:
                 article = Article(article_link, config=self.config)
                 article.download()
                 article.parse()
-                article.nlp()
-                record_dict['Date'] = temp_df['date'][ind]
-                record_dict['Media'] = temp_df['media'][ind]
-                record_dict['Title'] = article.title
-                record_dict['Article'] = article.text
-                record_dict['Summary'] = article.summary
-                record_dict['Link'] = article_link
+                record_dict = { 'Date': temp_df['date'][ind],
+                                'Media': temp_df['media'][ind],
+                                'Title':article.title,
+                                'Article': article.text,
+                                'Link': article_link }
                 content_list.append(record_dict)
             except:
                 print('Can\'t fetch article: {:s}'.format(temp_df['link'][ind]))
         self.news_df = pd.DataFrame(content_list)
         self.news_df['Date'] = pd.to_datetime(self.news_df.Date).dt.date
         self.news_df = self.news_df.sort_values(by='Date', ignore_index=True)
-        
+        self.news_df.reset_index()
+    
+    def search_stock_price(self):
+        self.price_df = yf.download(self.ticker,
+                                    start=self.start_time,
+                                    end=self.end_time,
+                                    group_by='ticker')
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from DataCollector import *
 
 if __name__ == '__main__':
-    data_collector = DataCollector(keywords_list =['JPM'],
-                                   start_time='05/01/2012',
-                                   end_time='05/02/2012')
+    data_collector = DataCollector(ticker = 'JPM',
+                                   keywords_list =['JPM'],
+                                   start_time='2012-05-01',
+                                   end_time='2012-05-02')
     data_collector.search_news()
     data_collector.news_df.to_csv('test.csv', index=False)
